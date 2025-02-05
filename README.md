b666613d6fde1602cfd29e559149707e4de793cb
Mislav Čačić <mcacic99@gmail.com>
2025-02-05 22:10:51 +0100

    Projektni zadatak iz kolegija Metode i tehnike testiranja programske podrške

---
+++ README.md
@@ -0,0 +1,32 @@
+# README – Selenium Testiranje za DemoBlaze
+
+## Pregled
+Projekt sadrži automatizirane **Selenium** testove za web stranicu **Swag Labs**. 
+Skripta testira funkcionalnosti stranice: prijava korisnika (defaultni podaci koji su dani na stranici), dodavanje prvog itema u košaricu, odlazak u košaricu, uklanjanje itema iz košarice, vraćanje na početnu stranicu i odjavu korisnika sa stranice.
+
+## O Swag Labs stranici
+[Swag Labs](https://www.saucedemo.com/) je **dummy web trgovina** namijenjena za testiranje i vježbanje automatizacije testiranja. Stranica simulira funkcionalnosti e-trgovine, uključujući:
+- Registraciju i prijavu korisnika
+- Pregled proizvoda
+- Dodavanje proizvoda u košaricu
+- Uklanjanje proizvoda iz košarice
+- Proces kupnje (Unos podataka za dostavu)
+- Sortiranje proizvoda
+
+## Korišteni alati
+Za izradu i izvođenje testova korišteni su alati:
+- **Python** – programski jezik korišten za pisanje testova
+- **Selenium WebDriver** – alat za automatizaciju web preglednika
+- **Google Chrome** – preglednik korišten za testiranje
+- **Chrome WebDriver** – potreban za kontrolu preglednika putem Seleniuma
+
+## Napomena
+Koristi webdriver_manager za automatsko preuzimanje i postavljanje ChromeDrivera. Razlog tome jest što sam kod pisao na MacOS-u međutim htio sam i mogućnost pokretanja na Windows OS.
+Ne koristi platform-specifične putanje, odnosno koristi se Service(ChromeDriverManager().install()) koji se automatski brine o tome.
+
+## Preduvjeti
+Prije pokretanja skripte potrebno je instalirati **Selenium** pomoću sljedeće naredbe:
+
+```bash
+pip install selenium
+python3 -m pip install selenium
---
+++ Testiranje.py
@@ -0,0 +1,126 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def setup():
+    chromeOptions = Options()
+    chromeOptions.add_argument("--start-fullscreen")
+    
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chromeOptions)
+    driver.maximize_window()
+    return driver
+
+#Prijava korisnika
+def testLogin(driver):
+    driver.get("https://www.saucedemo.com/")
+    driver.implicitly_wait(10)
+    time.sleep(1)
+    
+    usernameField = driver.find_element(By.ID, "user-name")
+    usernameField.send_keys("problem_user")
+    time.sleep(1)
+
+    passwordField = driver.find_element(By.ID, "password")
+    passwordField.send_keys("secret_sauce")
+    time.sleep(1)
+
+    loginButton = driver.find_element(By.ID, "login-button")
+    loginButton.click()
+    time.sleep(1)
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "inventory_list")))
+    print("Login test passed!")
+
+#Dodavanje prvog artikla u košaricu 
+def testAddToCart(driver):
+    time.sleep(1)
+
+    addToCartButton = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "(//button[contains(text(), 'Add to cart')])[1]"))
+    )
+
+    addToCartButton.click()
+    time.sleep(1)
+
+    print("Add to cart test passed!")
+
+#Otvaranje košarice i provjera postoji li artikl u košarici
+def testViewCart(driver):
+    time.sleep(1)
+
+    cartButton = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "shopping_cart_link"))
+    )
+
+    cartButton.click()
+    time.sleep(1)
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "cart_list")))
+
+    print("View cart test passed!")
+
+#Uklanjanje prvog artikla koji je u košarici
+def testRemoveFromCart(driver):
+    time.sleep(1)
+
+    removeButton = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "(//button[contains(text(), 'Remove')])[1]"))
+    )
+
+    removeButton.click()
+    time.sleep(1)
+
+    print("Remove from cart test passed!")
+
+#Povratak na main page
+def testReturnToHome(driver):
+    time.sleep(2)
+    continueShoppingButton = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "continue-shopping"))
+    )
+    continueShoppingButton.click()
+    time.sleep(1)
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "inventory_list")))
+
+    print("Return to home test passed!")
+
+#Odjava korisnika
+def testLogout(driver):
+    time.sleep(1)
+
+    menuButton = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "react-burger-menu-btn"))
+    )
+
+    menuButton.click()
+    time.sleep(1)
+    
+    logoutButton = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "logout_sidebar_link"))
+    )
+
+    logoutButton.click()
+    time.sleep(3)
+    
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "login-button")))
+
+    print("Logout test passed!")
+
+if __name__ == "__main__":
+    driver = setup()
+    try:
+        testLogin(driver)
+        testAddToCart(driver)
+        testViewCart(driver)
+        testRemoveFromCart(driver)
+        testReturnToHome(driver)
+        testLogout(driver)
+    finally:
+        driver.quit()
